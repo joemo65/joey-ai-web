d4a708bd56e337392f76b467135bb6a131d13dde
Joe Moore <joemo65@gmail.com>
2023-03-06 19:40:10 -0800

    removed react link

--- joey-ai-web/src/App.js
+++ joey-ai-web/src/App.js
@@ -9,14 +9,6 @@ function App() {
         <p>
           Joey AI Coming Soon!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
